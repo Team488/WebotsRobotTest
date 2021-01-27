ac7e6b91af8189f6002116e5ed83937e44edd9fd
JohnGilb <johngilb@gmail.com>
2021-01-26 20:34:46 -0800

    Configure the robot with gyro and encoders (#6)

--- src/main/java/competition/operator_interface/OperatorInterface.java
+++ src/main/java/competition/operator_interface/OperatorInterface.java
@@ -18,7 +18,7 @@ public class OperatorInterface {
     @Inject
     public OperatorInterface(CommonLibFactory factory, RobotAssertionManager assertionManager) {
         gamepad = factory.createXboxController(0);
-        gamepad.setLeftInversion(false, false);
-        gamepad.setRightInversion(false, false);
+        gamepad.setLeftInversion(false, true);
+        gamepad.setRightInversion(true, true);
     }
 }
