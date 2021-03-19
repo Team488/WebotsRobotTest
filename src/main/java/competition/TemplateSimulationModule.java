144f4ac6d16336db36b686795d20b90d5c9a4e48
John Gilbert <johngilb@gmail.com>
2021-03-18 18:34:21 -0700

    Fix up the simulation module

--- src/main/java/competition/TemplateSimulationModule.java
+++ src/main/java/competition/TemplateSimulationModule.java
@@ -1,7 +1,9 @@
 package competition;
 
+import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
 import xbot.common.injection.SimulatorModule;
+import xbot.common.subsystems.drive.BaseDriveSubsystem;
 import xbot.common.subsystems.pose.BasePoseSubsystem;
 
 
@@ -17,5 +19,6 @@ public class TemplateSimulationModule extends SimulatorModule {
     protected void configure() {
         super.configure();
         this.bind(BasePoseSubsystem.class).to(PoseSubsystem.class);
+        this.bind(BaseDriveSubsystem.class).to(DriveSubsystem.class);
     }
 }
