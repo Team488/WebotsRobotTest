34668c0201df0740b25d98473e7855f338753184
John Gilbert <johngilb@gmail.com>
2021-08-21 22:09:34 -0700

    Improve circle logic

--- src/main/java/competition/subsystems/drive/commands/CircleDriveCommand.java
+++ src/main/java/competition/subsystems/drive/commands/CircleDriveCommand.java
@@ -9,6 +9,7 @@ import xbot.common.command.BaseCommand;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.math.FieldPose;
 import xbot.common.math.XYPair;
+import xbot.common.properties.BooleanProperty;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 import xbot.common.simulation.WebotsClient;
@@ -25,6 +26,7 @@ public class CircleDriveCommand extends BaseCommand {
     DoubleProperty drivePower;
     DoubleProperty spiralFactor;
     DoubleProperty spiralMax;
+    BooleanProperty terminalApproach;
 
     @Inject
     public CircleDriveCommand(DriveSubsystem drive, PoseSubsystem pose, WebotsClient webots, CommonLibFactory clf, PropertyFactory propertyFactory) {
@@ -38,12 +40,15 @@ public class CircleDriveCommand extends BaseCommand {
         drivePower = propertyFactory.createPersistentProperty("DrivePower", 0.25);
         spiralFactor = propertyFactory.createPersistentProperty("SpiralFactor", 1);
         spiralMax = propertyFactory.createPersistentProperty("SpiralMax", 15);
+        terminalApproach = propertyFactory.createEphemeralProperty("Terminal Approach", false);
     }
 
     @Override
     public void initialize() {
         log.info("Initializing");
     }
+
+    XYPair circleCenter;
 
     @Override
     public void execute() {
@@ -57,10 +62,30 @@ public class CircleDriveCommand extends BaseCommand {
         webots.drawLine("GoalAhead", ahead.getPoint(), goalPoint.getPoint(), Color.kBlue, 60);
         webots.drawLine("GoalBehind", behind.getPoint(), goalPoint.getPoint(), Color.kRed, 60);
 
-        XYPair circleCenter = goalPoint.getCenterOfCircleConnectingFieldPoseAndPoint(pose.getCurrentFieldPose().getPoint());
+        // Needs some kind of "terminal" lock, where once we are close enough the circle stays fixed and we just follow the route.
+        if (circleCenter == null) {
+            circleCenter = goalPoint.getCenterOfCircleConnectingFieldPoseAndPoint(webots.getTruePosition());        
+        }
+        if (circleCenter != null) {
+            double distanceToGoal = webots.getTruePosition().getDistanceToPoint(goalPoint.getPoint());
+            if (distanceToGoal < 30) {
+                // Terminal approach. Don't update circle
+                terminalApproach.set(true);
+            } else {
+                terminalApproach.set(false);
+                // Still far away. Keep updating circle
+                circleCenter = goalPoint.getCenterOfCircleConnectingFieldPoseAndPoint(webots.getTruePosition());
+            }
+        }
+
+        // Figure out which side the circle is on. If it's to the "right" of the goal point, we need to follow clockwise.
+        // If it's to the "left" of the goal point, we need to follow counter-clockwise.
         
         FieldPose truePose = new FieldPose(webots.getTruePosition(), pose.getCurrentHeading().clone());
         double distanceFromCenter = webots.getTruePosition().getDistanceToPoint(circleCenter);
+
+        boolean clockwise = goalPoint.getRelativeAngleToPoint(circleCenter) < 0;
+        double extraHeadingToFollowCircle = clockwise ? -90 : 90;
 
         webots.drawCircle("MainCircle", circleCenter, (float)(distanceFromCenter / BasePoseSubsystem.INCHES_IN_A_METER), Color.kGreen, 60);
         
@@ -68,7 +93,7 @@ public class CircleDriveCommand extends BaseCommand {
 
         //double circleAngle = webots.getTruePosition().clone().getAngle();
         double circleAngle = circleCenter.getAngleToPoint(truePose.getPoint());
-        double idealAngle = circleAngle + 90;
+        double idealAngle = circleAngle + extraHeadingToFollowCircle;
         //double distanceFromCenter = webots.getTruePosition().getDistanceToPoint(new XYPair(0,0));
         //double distanceFromCenter = webots.getTruePosition().getDistanceToPoint(circleCenter);
         double spiralAdjust = 0;//(distanceFromCenter - 5*12)*spiralFactor.get();
